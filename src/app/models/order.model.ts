9e387f62879e98464b4aa861f83adb8631954510
Ezgi Buse Parlakçı <33104218+EzgiBuse@users.noreply.github.com>
2023-11-07 14:12:04 +0300

    Angular UI

---
+++ src/app/models/order.model.ts
@@ -0,0 +1,8 @@
+export interface Order{
+    OrderID:string,
+    DealerId:string,
+    OrderDate:number,
+    Status:number,
+    PaymentMethod:number,
+    TotalAmount:number
+}
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    ProductID:string,
+    ProductName:string,
+    Price:number,
+    StockQuantity:number,
+    MinimumStock:number
+}
