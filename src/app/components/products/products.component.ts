2e42a904b6cfd0eff09076cd4fe026ad7a4a09fc
Ezgi Buse Parlakçı <33104218+EzgiBuse@users.noreply.github.com>
2023-11-07 14:07:02 +0300

    Add files via upload

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  
+  
+   
+  }
+  
+ 
+
+
+
+
